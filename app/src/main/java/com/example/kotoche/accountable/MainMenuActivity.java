5543a0a719a8f381af815fc86e2f2ff391a73a70
Koniev <gautier.carree@isen-lille.fr>
2017-01-12 21:35:30 +0100

    +mod MainMenuActivity +mod main_menu.xml

--- app/src/main/java/com/example/kotoche/accountable/MainMenuActivity.java
+++ app/src/main/java/com/example/kotoche/accountable/MainMenuActivity.java
@@ -2,22 +2,19 @@ package com.example.kotoche.accountable;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainMenuActivity extends AppCompatActivity {
-    private TextView text = null;
+public class MainMenuActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String applicationName = getResources().getString(R.string.app_name);
+
         TextView text = new TextView(this);
+        applicationName = applicationName.replace("Application", "Testing app");
         text.setText("Welcome, this is an app to manage your budget");
+        text.setText(applicationName);
+
         setContentView(text);
     }
 
